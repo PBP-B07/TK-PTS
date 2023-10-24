08da1923436be591fa45906749db1121860a30e1
Hanan Adipratama <hananadipratama21@gmail.com>
2023-10-24 22:10:31 +0700

    update header

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    date_added = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
+    description = models.TextField()
