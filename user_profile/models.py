652e620fbafb6699a7e8c33b9f116655a5bdb519
Hanan Adipratama <hananadipratama21@gmail.com>
2023-10-25 23:01:41 +0700

    update prototype model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class UlasanBuatan(models.Model):
+    rate = models.IntegerField()
+    description = models.TextField()
+
+class ForumBuatan(models.Model):
+    rate = models.IntegerField()
+    description = models.TextField()
+
+class BukuBuatan(models.Model):
+    rate = models.IntegerField()
+    description = models.TextField()
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
+from user_profile.models import Profile, UlasanBuatan
+from django.core.management import call_command
 
 # Create your views here.
 def show_main(request):
@@ -7,3 +11,22 @@ def show_main(request):
     }
 
     return render(request, "profile.html", context)
+
+def get_profile(request):
+    data = UlasanBuatan.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def get_ulasan(request):
+    data = UlasanBuatan.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def load_my_initial_data(apps, schema_editor):
+    call_command("loaddata","fixtures/data.json")
+
+def get_profile_json(request):
+    product_item = Profile.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+def get_ulasan_json(request):
+    product_item = UlasanBuatan.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
