0dfe5f39da65a06bb106d5b18913719982f648d1
LokSki22 <muhammad.farrel21@ui.ac.id>
2023-10-25 13:47:43 +0700

    catalogue update

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'page': 'catalogue',
+    }
+
+    return render(request, "show_catalogue.html", context)
