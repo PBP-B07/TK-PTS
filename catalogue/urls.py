3cc35f920337217c69050ddbdfe78d33acfc2f93
LokSki22 <muhammad.farrel21@ui.ac.id>
2023-10-26 15:48:34 +0700

    katalog sort(non-ajax)

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalogue.views import show_main
 
+
 app_name = 'catalogue'
 
 urlpatterns = [
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from book.models import Book
+
+
 
 # Create your views here.
 def show_main(request):
+    if 'sort' in request.GET and request.GET['sort'] == 'title':
+        books = Book.objects.all().order_by('title').values()
+    else:
+        books = Book.objects.all().values()
+
     context = {
         'page': 'catalogue',
+        'books': books,
     }
 
     return render(request, "show_catalogue.html", context)
+
+
+
+
