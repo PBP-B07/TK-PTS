3d4ac9a061f2f85bf62c4c2dd76a45673332ba28
azmifal <kunazmifalah@gmail.com>
2023-10-27 00:03:58 +0700

    admin registered

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from book.views import get_books, show_main_book
+from book.views import get_books, get_reviews, show_main_book
 
 app_name = 'book'
 
 urlpatterns = [
     path('', get_books, name='get_books'),
     path('book/<int:book_id>/', show_main_book, name='show_main_book'),
+    path('review/<int:book_id>/', get_reviews, name='get_reviews'),
 ]
--- book/views.py
+++ book/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.core import serializers
 
 from book.models import Book
+from reviews.models import Review
 
 def show_main_book(request, book_id):
     
@@ -18,3 +19,9 @@ def show_main_book(request, book_id):
 def get_books(request):
     data = Book.objects.all()
     return HttpResponse(serializers.serialize("json", data),content_type="application/json")
+
+def get_reviews(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    reviews = Review.objects.filter(book=book)
+    return HttpResponse(serializers.serialize("json", reviews),content_type="application/json")
+
