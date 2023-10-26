ec82a785f7acf784c40ed8dd47f437d00622cd63
LokSki22 <muhammad.farrel21@ui.ac.id>
2023-10-26 20:56:00 +0700

    search bar ajax finished + dan udah sorted by title abjad

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from book.models import Book
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.core import serializers
+from django.db.models import Q  # jangan lupa mengimpor Q
+from django.http import JsonResponse  # impor JsonResponse
 
 
 
 # Create your views here.
 def show_main(request):
-    if 'sort' in request.GET and request.GET['sort'] == 'title':
-        books = Book.objects.all().order_by('title').values()
-    else:
-        books = Book.objects.all().values()
+    # if 'sort' in request.GET and request.GET['sort'] == 'title':
+    #     books = Book.objects.all().order_by('title').values()
+    # else:
+    #     books = Book.objects.all().order_by('title').values()
 
     context = {
         'page': 'catalogue',
-        'books': books,
+        # 'books': books,
     }
 
     return render(request, "show_catalogue.html", context)
@@ -23,6 +25,17 @@ def show_main(request):
 
 
 
+
+
 def get_product_json(request):
-    product_item = Book.objects.all()
-    return HttpResponse(serializers.serialize('json', product_item))
+    query = request.GET.get('query', '')  # mendapatkan query pencarian dari parameter GET
+    # Jika ada query pencarian, filter buku yang judulnya mengandung query
+    if query:
+        books = Book.objects.filter(title__icontains=query).order_by('title')
+    else:
+        # Jika tidak ada query pencarian, ambil semua buku dan urutkan berdasarkan judul
+        books = Book.objects.all().order_by('title')
+    
+    # Mengonversi queryset ke format JSON dan mengembalikannya sebagai respons
+    books_json = serializers.serialize('json', books)
+    return HttpResponse(books_json)
