f5c370e0ac698d656115a30ddccda0a789f57fe7
azmifal <kunazmifalah@gmail.com>
2023-10-27 17:31:31 +0700

    adding ajax for showing reviews

--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 
 from book.models import Book
@@ -20,8 +20,20 @@ def get_books(request):
     data = Book.objects.all()
     return HttpResponse(serializers.serialize("json", data),content_type="application/json")
 
+# def get_reviews(request, book_id):
+#     book = get_object_or_404(Book, pk=book_id)
+#     reviews = Review.objects.filter(book=book)
+#     return HttpResponse(serializers.serialize("json", reviews),content_type="application/json")
+
 def get_reviews(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    reviews = Review.objects.filter(book=book)
-    return HttpResponse(serializers.serialize("json", reviews),content_type="application/json")
+    reviews = Review.objects.filter(book_id=book_id)[:5]  # Mengambil 5 review pertama
+    review_data = [{'text': review.description, 'author': review.user} for review in reviews]
+    return JsonResponse(review_data, safe=False)
+
+def show_reviews(request, book_id):
+    reviews = Review.objects.filter(book_id=book_id)
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, "reviews.html", context)
 
