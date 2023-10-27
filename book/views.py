7b38d9a7bc2fb58ff3260ace5305867f7933a687
azmifal <kunazmifalah@gmail.com>
2023-10-27 22:55:22 +0700

    tampilan deskripsi buku sementara

--- book/views.py
+++ book/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 
@@ -19,6 +20,24 @@ def show_main_book(request, book_id):
 def get_books(request):
     data = Book.objects.all()
     return HttpResponse(serializers.serialize("json", data),content_type="application/json")
+
+@csrf_exempt
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+
+    if request.method == 'POST':
+        # Ambil data yang diedit dari permintaan POST
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+
+        # Edit data buku
+        book.name = name
+        book.description = description
+        book.save()
+
+        return HttpResponse(status=201)  # Berhasil mengedit buku
+
+    return HttpResponse(status=400)  # Gagal mengedit buku
 
 # def get_reviews(request, book_id):
 #     book = get_object_or_404(Book, pk=book_id)
