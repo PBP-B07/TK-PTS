197f8ff1af2c40ec316c86fa1e0fa8e2accb226c
Hanan Adipratama <hananadipratama21@gmail.com>
2023-10-28 00:23:22 +0700

    read, delete review

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user_profile.views import show_main, get_profile_json, get_profile, edit_profile_ajax
+from user_profile.views import show_main, get_profile_json, get_profile, edit_profile_ajax, get_reviews, delete_review, get_review_json
 
 app_name = 'user_profile'
 
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('get-profile/', get_profile_json, name='get_profile_json'),
     path('get/', get_profile, name = 'get_profile'),
-    path('edit/', edit_profile_ajax, name='edit_profile_ajax')
+    path('edit/', edit_profile_ajax, name='edit_profile_ajax'),
+    path('reviews/', get_reviews, name='get_reviews'),
+    path('delete_review/<int:id>/', delete_review, name='delete_review'),
+    path('get_review/<int:id>/', get_review_json, name='get_review_json'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.core import serializers
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from user_profile.models import Profile
+from reviews.models import Review
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -20,6 +22,7 @@ def get_profile(request):
     profile = Profile.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('json', profile))
 
+@csrf_exempt
 def edit_profile_ajax(request):
     if request.method == 'POST':
         name = request.POST.get("name")
@@ -33,3 +36,20 @@ def edit_profile_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def delete_review(request, id):
+    if request.method == 'POST':
+        review = Review.objects.get(pk=id)
+        review.delete()
+        return HttpResponse("Review deleted successfully", status=200)
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+def get_reviews(request):
+    reviews = Review.objects.filter(user=request.user).values('book__title', 'book__pk', 'pk', 'description', 'star', 'date_added')
+
+    return JsonResponse(list(reviews), safe=False)
+
+def get_review_json(request, id):
+    review = Review.objects.filter(pk = id).values('book__title', 'book__pk', 'pk', 'description', 'star', 'date_added')
+    return JsonResponse(list(review), safe=False)
