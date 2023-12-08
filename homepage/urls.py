f0f2fed1c4f75fb70e17c2fc18b2a93d2c87b321
monicaa37 <monikadamanik37@gmail.com>
2023-12-09 00:20:47 +0700

    autentikasi homepage

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,6 @@
 from book.views import show_main_book
 from django.urls import path
-from homepage.views import get_event, show_main, create_event,get_busiest_forum
+from homepage.views import get_event, show_main, get_busiest_forum
 from homepage.views import get_book, get_event, add_event_ajax, get_review,get_categories_json,get_forum
 from homepage.views import get_recomended_forum, get_not_recomended_forum
 
@@ -8,7 +8,6 @@ app_name = 'homepage'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create_event/', create_event, name='create_event'),
     path('get_book/',get_book,name='get_book'),
     path('book/<int:book_id>/', show_main_book, name='book_detail'),
     path('get_event/', get_event,name='get_event'),
--- homepage/views.py
+++ homepage/views.py
@@ -13,9 +13,9 @@ from forum.models import Forum
 from django.db.models import Q
 
 # Create your views here.
+# fungsi show main : akan melakukan autentifikasi last login 
 @login_required(login_url='autentifikasi/login')
 def show_main(request):
-    
     form = HomepageForm()
     
     if 'last_login' not in request.COOKIES:
@@ -28,30 +28,10 @@ def show_main(request):
         'pk' : request.user.pk,
         'book' : Book.objects.all(),
         'form': form
-        
     }
-
     return render(request, "main.html", context)
-   
-@csrf_exempt
-@login_required(login_url='autentifikasi/login')
-def create_event(request):
-    if request.method == 'POST':
-        title = request.POST.get('title', '')
-        description = request.POST.get('description', '')
 
-        # Memastikan bahwa pengguna yang membuat event adalah admin
-        if not request.user.is_staff:
-            return JsonResponse({'status': 'error', 'message': 'Hanya admin yang dapat membuat event.'})
-
-        # Simpan event ke database
-        event = BookEvent.objects.create(title=title, description=description, book_related=True)
-        event.save()
-
-        return JsonResponse({'status': 'success', 'message': 'Event berhasil disimpan.'})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Metode permintaan tidak valid.'})
-
+# fungsi add event ajax : akan membuat event ajax dengan mengambil berupa title, description, dan book title 
 @csrf_exempt
 @login_required(login_url='autentifikasi/login')
 def add_event_ajax(request):
@@ -68,6 +48,11 @@ def add_event_ajax(request):
 
     return HttpResponseNotFound()
 
+
+# get latest review : mengambil data dari model Review.
+# Data diurutkan berdasarkan bidang 'star' secara menurun dan memilih 3 ulasan teratas.
+# Data yang diambil kemudian dikonversi menjadi respons JSON menggunakan kelas JsonRespons
+# Respons JSON yang dihasilkan akan berisi informasi tentang 3 ulasan teratas, user, star, description dan book title 
 @login_required(login_url='autentifikasi/login')
 def get_review(request):
     review = Review.objects.order_by('-star')[:3].values(
@@ -75,16 +60,24 @@ def get_review(request):
     )
     return JsonResponse(list(review), safe=False)
 
+
+# fungsi get book : akan mengambil semua objek book 
 @login_required(login_url='autentifikasi/login')
 def get_book(request):
     book = Book.objects.all()
     return HttpResponse(serializers.serialize("json",book))
 
-@login_required(login_url='autentifikasi/login')
+
+# get event : pertama, fungsi ini mengambil semua data dari model BookEvent
+# lalu, mengambil values 'book__title', 'title', 'description', 'book', dan 'book__pk'.
+# dan dikembalikan dengan respon json 
+#@login_required(login_url='autentifikasi/login')
 def get_event(request):
     bookEvent = BookEvent.objects.all().values('book__title','title','description','book', 'book__pk')
     return JsonResponse(list(bookEvent), safe=False)
 
+
+# fungsi get product json : akan memisahkan berdasarkan book kategori
 @login_required(login_url='autentifikasi/login')
 def get_product_json(request):
     query = request.GET.get('query', '')  # mendapatkan query pencarian dari parameter GET
@@ -114,7 +107,11 @@ def get_categories_json(request):
     return JsonResponse(list(categories), safe=False)
 
 
-# get latest forum for one user
+# get latest forum for one user : fungsi ini akan mengambil semua data dari model Forum
+# Data diurutkan berdasarkan tanggal penambahan ('date_added') secara menurun (dari yang paling baru).
+# Hanya dua forum teratas yang dipilih menggunakan slicing [:2]
+# Lalu, Respons JSON yang dihasilkan akan berisi informasi tentang dua forum terbaru, termasuk nama pengguna,
+# subject, user, description 
 @login_required(login_url='autentifikasi/login')
 def get_forum(request):
     forum = Forum.objects.order_by('-date_added')[:2].values(
@@ -122,7 +119,12 @@ def get_forum(request):
     return JsonResponse(list(forum), safe=False)
 
 
-# get forum busiest for one user
+# get forum busiest for one user : fungsi ini akan mengambil data dari model Forum
+# dan memfilter forum berdasarkan pengguna yang saat ini masuk
+# kemudian, hasilnya diurutkan berdasarkan jumlah balasan ('total_reply') secara menurun, 
+# dan hanya dua forum teratas yang dipilih menggunakan slicing [:2] 
+# dan akan dikembalikan dengan respon json yang berisi informasi tentang dua forum terbusiest
+# yang dibuat oleh pengguna yang saat ini masuk, subject, description, 
 @login_required(login_url='autentifikasi/login')
 def get_busiest_forum(request):
     forum = Forum.objects.filter(user=request.user).order_by('-total_reply')[:2].values(
@@ -130,7 +132,9 @@ def get_busiest_forum(request):
     return JsonResponse(list(forum), safe=False)
 
 
-# get recomended forum for all user
+# get recomended forum for all user : akan mengambil data forum yang direkomendasikan berdasarkan paling banyak peminatnya dari seluruh user
+# untuk mengambil data forum yang paling banyak peminat dapat dilihat dari jumlah replies forum tersebut 
+# fungsi ini akan mengambil 2 forum yang paling direkomen dengan mengambil values title, username, subject, description
 @login_required(login_url='autentifikasi/login')
 def get_recomended_forum(request):
     forum = Forum.objects.order_by('-total_reply')[:2].values(
@@ -138,7 +142,9 @@ def get_recomended_forum(request):
     return JsonResponse(list(forum), safe=False)
 
 
-# get not recomended forum for all user
+# get not recomended forum for all user : akan mengambil data forum yang tidak direkomendasikan berdasarkan kurang peminatnya dari seluruh user
+# untuk mengambil data forum yang kurang peminat dapat dilihat dari jumlah replies forum tersebut 
+# fungsi ini akan mengambil 2 forum yang tidak direkomen dengan mengambil values title, username, subject, description
 def get_not_recomended_forum(request):
     forum = Forum.objects.order_by('total_reply')[:2].values(
         "user","user__username", "date_added", "subject", "description", "pk", "book__title", "total_reply", 'book__pk')
