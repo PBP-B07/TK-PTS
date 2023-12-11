e16c0c3deaf41149d119081de3f646320e41f732
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2023-12-12 01:15:21 +0700

    menambahkan function add review flutter

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from reviews.views import add_reviews_ajax, get_reviews_json, get_user_review, show_review
+from reviews.views import add_reviews_ajax, get_reviews_json, get_user_review, show_review, add_reviews_flutter
 
 app_name = 'reviews'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:id>', show_review, name='show_review'),
     path('get-reviews-json/<int:id>/', get_reviews_json, name='get_reviews_json'),
     path('create-reviews-ajax/<int:id>/', add_reviews_ajax, name='add_reviews_ajax'), 
-    path('get-user-reviews/<int:id>', get_user_review, name='get_user_review'),
+    path('get-user-reviews/<int:id>/', get_user_review, name='get_user_review'),
+    path('create-reviews-flutter/<int:id>/', add_reviews_flutter, name='add_reviews_flutter')
 ]
