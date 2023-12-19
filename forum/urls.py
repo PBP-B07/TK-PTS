177f09de3a94874ffc48ba676c726fc74cc4c6dc
rahidasyf <rahidanona74@gmail.com>
2023-12-19 18:16:33 +0700

    forum done

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from forum.views import create_forum_flutter
+from forum.views import create_forum_flutter, create_reply_flutter
 from forum.views import show_forum
 from forum.views import get_forum_json, add_forum_ajax, get_reply_json, add_reply_ajax
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('<int:bookid>/get-reply/<int:id>/', get_reply_json, name='get_reply_json'),
     path('create-forum-ajax/<int:id>/', add_forum_ajax, name='add_forum_ajax'),
     path('<int:bookid>/create-reply-ajax/<int:id>/', add_reply_ajax, name='add_reply_ajax'),
-    path('create-flutter/', create_forum_flutter, name='create_forum_flutter'),
+    path('create-flutter/<int:id>/', create_forum_flutter, name='create_forum_flutter'),
+    path('<int:bookid>/create-reply-flutter/<int:id>/', create_reply_flutter, name='create_reply_flutter'),
     
 ]
